c811c2162eb31826572a5142893487ec9f7e481b
nox <samingosi97@gmail.com>
2023-10-12 17:36:36 -0700

    recode strtok

--- main.h
+++ main.h
@@ -18,7 +18,7 @@ char **splitInput(char *string);
 int checkinbuilt(char **string, char *inputString);
 char *fullpath(char *command_Idx);
 int executeFile(char **command);
+char *_strpbrk(char *s, char *accept);
+char *_strtok(char *str, const char *delim);
 
 #endif /*MAIN_H*/
-
-

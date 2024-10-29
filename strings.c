f9ed48b686af82ef4f5b499ec86c642fe3c2afdb
noxcodes <samingosi97@gmail.com>
2024-10-29 08:45:34 +0300

    add strcpy function

--- strings.c
+++ strings.c
@@ -14,3 +14,18 @@ int _strlen(char *str)
 		length++;
 	return (length);
 }
+/**
+ * _strcpy - copy a string to a new address
+ * @src: address of the string to be copied
+ * @dest: destination of the new string
+ * Return: pointer to the new address
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
